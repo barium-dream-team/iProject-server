c5be90c2c66c05eca030ea2df82c5f24e02c3ed4
khanz1 <assistance.xavier@gmail.com>
2023-02-07 09:56:23 +0700

    login udah ges

--- app.js
+++ app.js
@@ -4,5 +4,16 @@ app.get('/', (_, res) => {
     res.json({ message: 'Hello Bang' })
 })
 
+app.post('/login', async (req, res) => {
+    const {email, pass} = req.body;
+    if (!email || !pass) {
+        res.status(400).json({ message: 'yang bener aja dah' });
+        return;
+    }
+
+    const user = await User.findOne({ where: { email } })
+
+    res.json({ access_token: 'blablabla', user })
+})
 
 app.listen(3000, () => console.log('running bwang'))

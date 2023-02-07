11fb3896b97aeb808baea14714424a1d7ad8703e
khanz1 <assistance.xavier@gmail.com>
2023-02-07 10:01:35 +0700

    regis done

--- app.js
+++ app.js
@@ -4,5 +4,17 @@ app.get('/', (_, res) => {
     res.json({ message: 'Hello Bang' })
 })
 
+app.post('/register', async (req, res) => {
+    const { email, password, address } = req.body;
+
+    if (!email || !password || !address) {
+        res.status(400).json({ message: 'ah' })
+        return;
+    }
+
+    await User.create({ email, password, address })
+    res.status(201).json({ message: 'nice' });
+})
+
 
 app.listen(3000, () => console.log('running bwang'))
